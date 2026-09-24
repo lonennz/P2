e5fee4168513790a557860847a2974108fde0381
Paulo Islan <pauloislan2003@gmail.com>
2026-09-23 21:07:06 -0300

    atualização

---
+++ EX16.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class EX16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
+}
